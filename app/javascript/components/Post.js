ead55f12337409d7b3a1c668c2eec44efa4b0c98
Andrey Andreev <andrey.andreev303@gmail.com>
2017-10-26 11:02:45 +0300

    Add some font files and use them in the react component

--- app/javascript/components/Post.js
+++ app/javascript/components/Post.js
@@ -1,9 +1,15 @@
 import React from "react"
 import PropTypes from "prop-types"
+
+const PostStyles = {
+  fontFamily: "OpenSans",
+  border: "1px solid tomato"
+}
+
 class Post extends React.Component {
   render () {
     return (
-      <div>
+      <div style={PostStyles}>
         <p>Image path: {this.props.imagePath}</p>
         <img src={this.props.imagePath} alt="powpow"/>
         <div>Title: {this.props.title}</div>
