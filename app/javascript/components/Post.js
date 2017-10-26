a09be319fc0d4e7561a656d44c1437e856f6ab73
Andrey Andreev <andrey.andreev303@gmail.com>
2017-10-26 08:51:24 +0300

    Add image to asset pipeline and access it through within a component

--- app/javascript/components/Post.js
+++ app/javascript/components/Post.js
@@ -4,6 +4,8 @@ class Post extends React.Component {
   render () {
     return (
       <div>
+        <p>Image path: {this.props.imagePath}</p>
+        <img src={this.props.imagePath} alt="powpow"/>
         <div>Title: {this.props.title}</div>
         <div>Body: {this.props.body}</div>
       </div>
@@ -12,6 +14,7 @@ class Post extends React.Component {
 }
 
 Post.propTypes = {
+  imagePath: PropTypes.node,
   title: PropTypes.node,
   body: PropTypes.node
 };
